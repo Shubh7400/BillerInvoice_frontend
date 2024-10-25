c99bfc5be41514e989a418a669ca5f72dff4aee9
Prashant Soni <132332640+prashant9669@users.noreply.github.com>
2024-10-25 16:56:10 +0530

    dashboard design

--- src/components/Dashboard_Component/DashBoardPage.tsx
+++ src/components/Dashboard_Component/DashBoardPage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./dashboard.module.css";
 import clients from "../assets/client.png";
 import plane from "../assets/plane.gif";
+import projects from "../assets/project-task.png";
+import invoice from "../assets/invoice.png";
 import { Link } from "react-router-dom";
 
 function DashBoardPage() {
   return (
     <div>
-      <div>
+      <div className="flex gap-[30px]">
         <div className={`${styles.animated}`}>
           <div className={`${styles.count_total}`}>
             <h2>Total Clients</h2>
@@ -25,6 +27,30 @@ function DashBoardPage() {
           <div className={`${styles.tagline}`}>
             <p>Tracking Every Client,</p>
             <p> Every Time</p>
+          </div>
+        </div>
+
+        <div className={`${styles.total_projects}`}>
+          <div className={`${styles.project_section}`}>
+            <h2>Total Projects</h2>
+            <h1>6721</h1>
+          </div>
+          <div className={`${styles.project}`}>
+            <img src={projects} alt="" />
+          </div>
+          <div className={`${styles.tagline}`}>
+            <p>All Projects, One Dashboard for</p>
+            <p>Streamlined Success</p>
+          </div>
+        </div>
+      </div>
+
+      <div className=" flex gap-[40px]">
+        <div className={`${styles.invoice}`}>
+          <img src={invoice} alt="" />
+          <div className={`${styles.invoice_details}`}>
+            <h1>197</h1>
+            <h2>Genrated Invoice</h2>
           </div>
         </div>
       </div>
