a00bb6fd32bc613aa8f37bf933491c463ae1fe95
Prashant Soni <132332640+prashant9669@users.noreply.github.com>
2024-10-25 13:13:20 +0530

    design dashboard

--- src/components/Dashboard_Component/DashBoardPage.tsx
+++ src/components/Dashboard_Component/DashBoardPage.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import styles from "./dashboard.module.css";
+import clients from "../assets/client.png";
+import plane from "../assets/plane.gif";
+import { Link } from "react-router-dom";
 
 function DashBoardPage() {
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4">Dashboard Page</h2>
-      <p>List of clients will go here.</p>
+      <div>
+        <div className={`${styles.animated}`}>
+          <div className={`${styles.count_total}`}>
+            <h2>Total Clients</h2>
+            <h1>385</h1>
+          </div>
+          <div>
+            <img src={clients} alt="" />
+            <div className={`${styles.plane}`}>
+              <img src={plane} alt="" />
+            </div>
+          </div>
+          <div className={`${styles.viewBotton}`}>
+            <Link to="/">View All Clients</Link>
+          </div>
+          <div className={`${styles.tagline}`}>
+            <p>Tracking Every Client,</p>
+            <p> Every Time</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,26 @@
 import React, { useContext } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BsSun, BsMoon } from "react-icons/bs";
 import { AiOutlineLogout } from "react-icons/ai";
 import { AuthContext } from "../../states/context/AuthContext/AuthContext";
 import { ThemeContext } from "../../states/context/ThemeContext/ThemeContext";
+import { CgProfile } from "react-icons/cg";
 const Navbar = () => {
   const navigate = useNavigate();
   const { visibility, updateVisibility } = useContext(ThemeContext);
   return (
     <>
-      <nav className="h-18 w-[80vw] mb-0 text-colorDarkFont dark:text-colorLightFont bg-slate-50 dark:bg-slate-800 shadow-lg   dark:shadow-slate-950   flex justify-between p-4  items-center text-xl sticky top-0 ">
-        <div
+      <nav className="h-18 w-[85vw] mb-0 text-colorDarkFont dark:text-colorLightFont     flex justify-end p-4  items-center text-xl sticky top-0 ">
+        {/* <div
           className="mx-8 font-poppins-bold text-2xl text-thirdColor hover:text-thirdColorHover cursor-pointer tracking-widest "
           onClick={() => navigate("/")}
         >
           Biller
-        </div>
+        </div> */}
+        <Link to="/profile" className="text-gray-700 text-[35px] flex items-center gap-[10px] hover:text-blue-500">
+          <CgProfile />
+          </Link>
         {/* <div className="flex justify-between items-center ">
           <div
             className="mx-8 cursor-pointer "
