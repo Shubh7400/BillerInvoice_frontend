a04aa23db5bbd393cba5680714039109a071bdfe
prashant.soni@cubexo.io <prashant.soni@cubexo.io>
2024-11-05 14:50:04 +0530

    billing page

--- src/components/Invoice_Component/InvoicePage.tsx
+++ src/components/Invoice_Component/InvoicePage.tsx
@@ -5,6 +5,7 @@ import error from "../assets/select_client.png";
 import { useSelector } from "react-redux";
 import { RootState } from "../../states/redux/store";
 import { ClientType } from "../../types/types";
+import projectError from "../assets/project_error.png"
 
 const InvoicePage = () => {
   const navigate = useNavigate();
@@ -27,7 +28,7 @@ const InvoicePage = () => {
         ) : (
           <div className="flex justify-center items-center h-[80vh] m-auto">
             <div className="text-center">
-              <img src={error} alt="" className="w-[300px] m-auto" />
+              <img src={projectError} alt="" className="w-[300px] m-auto" />
               <h4 className="text-[20px] mb-3">
                 Please select atleast one project :{" "}
               </h4>
