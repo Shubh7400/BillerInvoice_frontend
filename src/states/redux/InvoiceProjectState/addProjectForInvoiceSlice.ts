f0672c54bd7d91a9b466a6c660296f18fce37e66
shubham <shubham.solanki@cubexo.io>
2024-11-05 17:46:10 +0530

    fix Project Table

--- src/states/redux/InvoiceProjectState/addProjectForInvoiceSlice.ts
+++ src/states/redux/InvoiceProjectState/addProjectForInvoiceSlice.ts
@@ -16,8 +16,15 @@ const addProjectForInvoiceSlice = createSlice({
   reducers: {
     addProjectForInvoiceAction: (state, action: PayloadAction<ProjectType>) => {
       return produce(state, (draftState: InitialStateType) => {
+        const { clientId, _id } = action.payload;
+        if (
+          draftState.projectsForInvoice.length > 0 &&
+          draftState.projectsForInvoice[0].clientId !== clientId
+        ) {
+          draftState.projectsForInvoice = [];
+        }
         const existingProject = draftState.projectsForInvoice.find(
-          (project) => project._id === action.payload._id
+          (project) => project._id === _id
         );
         if (!existingProject) {
           draftState.projectsForInvoice.push(action.payload);
